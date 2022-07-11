51c00e74c2cf7a175166824ca2361ac38d551460
zerofe13 <zerofe13@gmail.com>
2022-07-11 22:20:36 +0900

    Feat: Singup 추가

--- src/main/java/hello/instacloneproject/controller/LoginController.java
+++ src/main/java/hello/instacloneproject/controller/LoginController.java
@@ -1,14 +1,35 @@
 package hello.instacloneproject.controller;
 
+import hello.instacloneproject.domain.User;
+import hello.instacloneproject.repository.dto.UserLoginDto;
+import hello.instacloneproject.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.AbstractAuditable_;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
 
-    @GetMapping("/")
+    private final UserService userService;
+
+    @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model){
+//        model.addAttribute("user",new UserLoginDto());
+        return "signup";
+    }
+    @PostMapping("/signup")
+    public String signup(UserLoginDto userLoginDto){
+        userService.join(userLoginDto);
+        return "redirect:/login";
     }
 }
