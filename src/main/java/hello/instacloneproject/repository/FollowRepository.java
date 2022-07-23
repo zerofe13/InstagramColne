8f3c61012b4610360c6813a9799d5450aae3ed28
zerofe13 <zerofe13@gmail.com>
2022-07-24 00:36:46 +0900

    Refactor : fetch 조인

--- src/main/java/hello/instacloneproject/repository/FollowRepository.java
+++ src/main/java/hello/instacloneproject/repository/FollowRepository.java
@@ -18,19 +18,26 @@ public class FollowRepository {
         em.persist(follow);
     }
 
+
     public List<Follow> findByFollowingEmail(String email){
-        return em.createQuery("select f from Follow f where f.followingUser.email = :email",Follow.class)
+        return em.createQuery("select f from Follow f join fetch f.followingUser where f.followingUser.email = :email",Follow.class)
                 .setParameter("email",email)
                 .getResultList();
 
     }
+
+
 
     public List<Follow> findByFollowedEmail(String email){
-        return em.createQuery("select f from Follow f where f.followedUser.email = :email",Follow.class)
+        return em.createQuery("select f from Follow f join fetch f.followedUser where f.followedUser.email = :email",Follow.class)
                 .setParameter("email",email)
                 .getResultList();
 
     }
+
+//    public Follow findByFollowingIdAndFollowerId(long followingId,long followerId){
+//        em.createQuery("select f from Follow f ")
+//    }
 
     public void delete(Follow follow){
         em.remove(follow);
