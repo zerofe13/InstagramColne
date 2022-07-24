880c7bb6e27a3f659b98b10e76fdcd36494ec40d
zerofe13 <zerofe13@gmail.com>
2022-07-24 23:39:02 +0900

    Feat : 양방향 상태 변수 추가

--- src/main/java/hello/instacloneproject/domain/Follow.java
+++ src/main/java/hello/instacloneproject/domain/Follow.java
@@ -3,12 +3,13 @@ package hello.instacloneproject.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
-@Getter
+@Getter @Setter
 public class Follow {
     @Id
     @GeneratedValue
@@ -22,10 +23,13 @@ public class Follow {
     @ManyToOne
     private User followedUser;
 
+    private boolean bidirectional;
+
     @Builder
-    public Follow(User followingUser, User followedUser) {
+    public Follow(User followingUser, User followedUser,boolean bidirectional) {
         this.followingUser = followingUser;
         this.followedUser = followedUser;
+        this.bidirectional = bidirectional;
     }
 
 }
